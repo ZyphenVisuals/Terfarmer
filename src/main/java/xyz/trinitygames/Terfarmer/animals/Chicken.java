3155c70ae7b8bd82c7d4e88b7403df0e3a389b5a
ZyphenVisuals <rotarujunior@gmail.com>
2024-11-07 18:19:03 +0200

    refactored Animal to be an abstract class

--- src/main/java/xyz/trinitygames/Terfarmer/animals/Chicken.java
+++ src/main/java/xyz/trinitygames/Terfarmer/animals/Chicken.java
@@ -1,4 +1,27 @@
 package xyz.trinitygames.Terfarmer.animals;
 
-public class Chicken {
+public class Chicken extends Animal {
+    static final String[] random_names =  {"Henk", "Luna", "Nugget", "Cluck", "Blueburg"};
+    
+    public Chicken() {
+        this.name = random_names[(int) (Math.random() * random_names.length)];
+        // the lifetime of a chicken will be between 3 and 8 days
+        this.life = (int) (Math.random() * 6) + 3;
+        // the value of a chicken will be between 70 and 120
+        this.value = (int) (Math.random() * 51) + 70;
+        // the chicken will return its investment in 4 days
+        this.income = this.value / 4;
+    }
+    
+    /**
+     * Get the name of the animal.
+     * The format is "NAME (TYPE)".
+     *
+     * @return string representation of the name
+     */
+    @Override
+    public String getName() {
+        if(this.life > 0) return this.name + " (Chicken)";
+        return this.name + " (Chicken, Dead)";
+    }
 }
--- src/main/java/xyz/trinitygames/Terfarmer/main.java
+++ src/main/java/xyz/trinitygames/Terfarmer/main.java
@@ -1,4 +1,18 @@
 package xyz.trinitygames.Terfarmer;
 
+import xyz.trinitygames.Terfarmer.animals.Cow;
+import xyz.trinitygames.Terfarmer.animals.Animal;
+
 public class main {
+    public static void main(String[] args) {
+        Animal a = new Cow();
+        while(a.getLifetime() > 0) {
+            System.out.println(a.getName());
+            System.out.printf("Value: %d, income: %d\n", a.getValue(), a.getIncome());
+            a.age();
+        }
+        System.out.println(a.getName());
+        System.out.printf("Value: %d, income: %d\n", a.getValue(), a.getIncome());
+        a.age();
+    }
 }
